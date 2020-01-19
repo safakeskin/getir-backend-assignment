389110a1f9c6c87aa751b8c428496f2aed428084
Safa Keskin <keskinsaf@gmail.com>
2020-01-19 23:44:38 +0300

    feat(server.ts): add index handler

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,10 @@ app.use(express.json());
 
 app.use("/record", recordRuter);
 
+app.get('/', (req, res) => {
+    res.send("Hello, world!");
+})
+
 app.post('/', (req: express.Request, res: express.Response) => {
     const message = "In order to interact with api, please read README.md";
     res.send(message);
